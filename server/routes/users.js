82efd5cf02240f8a133d3c75445a08d2ecbe5efa
noobDevUmar <m.umarqureshi091@gmail.com>
2023-11-07 08:37:46 -0800

    auth

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,28 @@
 import express from 'express'
-import {} from "../controllers/user.js"
+import {deleteuser, dislikeVideo, getUser, likeVideo, sucribe, unsubcribe, update} from "../controllers/user.js"
 const router = express.Router()
+import { verifyToken } from '../VerifyToken.js'
+
+// update a User
+router.put("/:id",verifyToken,update)
+
+//delete a user
+router.delete("/:id",verifyToken,deleteuser)
+
+// get a user
+router.get("/:id",getUser)
+
+// subcribe a user
+router.put("/find/:id",sucribe)
+
+//unsubcribe a user
+router.put("/sub/:id",unsubcribe)
+
+//like a video
+router.put("/sub/:videoid",likeVideo)
+
+//dislike a video
+router.put("/:videoid",dislikeVideo)
 
 
 export default router;
